7832d82993a5379f1bab729b6d8ab68003bfb967
Zeek Miller <228266305+millertyme314-oss@users.noreply.github.com>
2026-05-16 09:32:28 -0400

    Enhance code readability with comments
    Added comments to clarify input variables and their purpose.

--- Lab-1/app.py
+++ Lab-1/app.py
@@ -1,10 +1,16 @@
+# Prints the greeting and title!
 print("What's the deal? Welcome to the Dev Team Name Generator!!!")
 
-
+# Introduces the 1st variable in the form of the input function 
+# User must enter the required information
+# "\n" serves as a line-break, to create a new line after input
 hometown = input("Stomping Grounds?\n ")
 
-
+# Introduces the 2nd variable in the form of the input function 
+# User must enter the required information
+# "\n" serves as a line-break, to create a new line after input
 specialty = input("Area of Expertise?\n " )
 
-
+# Prints a result string, then introduces a function call 
+# from both variables' input 
 print("Team name is:", hometown, specialty )
