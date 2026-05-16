2d2fe7cec0e47ff14112d95faca176035c7d5195
Zeek Millz <228266305+millertyme314-oss@users.noreply.github.com>
2026-05-15 21:39:13 -0400

    Lab-1 in here!

---
+++ Lab-1/app.py
@@ -0,0 +1,10 @@
+print("What's the deal? Welcome to the Dev Team Name Generator!!!")
+
+
+hometown = input("Stomping Grounds?\n ")
+
+
+specialty = input("Area of Expertise?\n " )
+
+
+print("Team name is:", hometown, specialty )
